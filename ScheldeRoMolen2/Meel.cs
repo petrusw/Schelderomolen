383adb2601327fb127bab426abfd24cdb57b17a5
Ward Petrus <petrus.ward@gmail.com>
2014-11-27 15:55:26 +0100

    27/11/2014 15:55

---
+++ ScheldeRoMolen2/Meel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScheldeRoMolen2
+{
+    public class Meel
+    {
+        private string naamValue;
+        public string Naam
+        {
+            get { return naamValue; }
+            set { naamValue = value; }
+        }
+
+        private decimal prijsValue;
+        public decimal Prijs
+        {
+            get { return prijsValue; }
+            set { prijsValue = value; }
+        }
+
+        public void Meel(string naam, decimal prijs)
+        {
+            Naam = naam;
+            Prijs = prijs;
+        }
+    }
+}
