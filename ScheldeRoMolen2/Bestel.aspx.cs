e6c09a7e9bbe53978693f2e15058c914f14e45df
Ward Petrus <petrus.ward@gmail.com>
2014-12-03 10:34:05 +0100

    3/12/2014 10.33

--- ScheldeRoMolen2/Bestel.aspx.cs
+++ ScheldeRoMolen2/Bestel.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml;
+using System.IO;
 
 namespace ScheldeRoMolen2
 {
@@ -17,6 +19,7 @@ namespace ScheldeRoMolen2
             LabelReqFieldTel.Text = "*";
             PanelType.Visible = false;
             PanelKlant.Visible = true;
+            readXml();
         }
 
         protected void ButtonVolgende_Click(object sender, EventArgs e)
@@ -31,6 +34,33 @@ namespace ScheldeRoMolen2
                 + TextBoxTel.Text+"<br/>"+ LabelDatumAfhaling.Text*/;
 
 
+        }
+
+        private void readXml()
+        {
+            string st = "";
+          using (XmlReader reader = XmlReader.Create(Server.MapPath("~/content/MeelSoorten.xml")))
+          {
+              while(reader.Read())
+              {
+                  if (reader.IsStartElement())
+                  {
+                      switch(reader.Name.ToString())
+                      {
+                          case "Naam":
+                              st = reader.ReadElementContentAsString();
+                              DropDownListSoort.Items.Add(st);
+                              break;
+
+                          case "Prijs":
+                              st += " "+reader.ReadElementContentAsString();
+                              break;
+                      }
+                  }
+                
+              }
+              
+          }
         }
     }
 }
--- ScheldeRoMolen2/Default.aspx
+++ ScheldeRoMolen2/Default.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/ScheldeRoMolen.Master" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="ScheldeRoMolen2._default" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/ScheldeRoMolen.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ScheldeRoMolen2._default" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
