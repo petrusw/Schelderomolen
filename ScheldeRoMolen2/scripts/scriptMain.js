0354d82692341cdcf5f26ee685e5fd146f1fd543
Ward Petrus <petrus.ward@gmail.com>
2015-01-05 14:13:07 +0100

    05/01/2015

--- ScheldeRoMolen2/scripts/scriptMain.js
+++ ScheldeRoMolen2/scripts/scriptMain.js
@@ -10,18 +10,32 @@ $(document).ready(function () {
     $(".button").hover(function () {
         $(this).removeClass("bigBorder2");
         $(this).addClass("bigBorder");
-        $(this).fadeTo(100, 1);
+       /* $(this).fadeTo(100, 1);*/
         $("topBarButton").addClass("textColorH3");
-        $(this).css("background-color", "#000000");
+        //$(this).css("background-color", "#000000");
 
     }, function () {
         $(this).removeClass("bigBorder");
         $(this).addClass("bigBorder2");
-        $(this).fadeTo(100, 0.5);
+       /* $(this).fadeTo(100, 0.5);*/
         $("topBarButton").removeClass("textColorH3");
-        $(this).css("background-color", "#100f0f");
+        //$(this).css("background-color", "#464040");
 
     }),
+    $(".button").focus(function () {
+        $(this).removeClass("bigBorder2");
+        $(this).addClass("bigBorder");
+       /* $(this).fadeTo(100, 1);*/
+        $("topBarButton").addClass("textColorH3");
+        //$(this).css("background-color", "#000000");
+    }, function () {
+        $(this).removeClass("bigBorder");
+        $(this).addClass("bigBorder2");
+        //$(this).fadeTo(100, 0.5);
+        $("topBarButton").removeClass("textColorH3");
+        //$(this).css("background-color", "#464040");
+    }),
+
     $("#footer").hover(function () {
         $(this).addClass("collapse");
     }, function(){
